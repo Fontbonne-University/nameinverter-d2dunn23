85cffade44c87fef61c43ec46a163b42ff72e424
Kevin Dunn <d2dunn51@gmail.com>
2021-03-23 01:29:13 -0500

    Extract Class not working for me so couldn't get classes out of Tests

--- nameInverter/src/nameInverter/nameInverterTest.java
+++ nameInverter/src/nameInverter/nameInverterTest.java
@@ -2,6 +2,10 @@ package nameInverter;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,10 +15,112 @@ public class nameInverterTest {
 	@Before
 	public void setUp() throws Exception {
 	}
+	
+	private void assertInverted(String originalName, String invertedName) {
+		assertEquals(invertedName, invertName(originalName));
+	}
 
 	@Test
 	public void nothing() {
 
+	}
+	
+	@Test
+	public void givenNull_returnsEmptyString() throws Exception{
+		assertInverted(null, "");
+	}
+
+
+	
+	@Test
+	public void givenEmptyString_returnEmptyString() throws Exception{
+		assertEquals("", invertName(""));
+	}
+	
+	@Test
+	public void givenSimpleName_returnSimpleName() throws Exception{
+		assertInverted("Name", "Name");
+	}
+	
+	@Test
+	public void givenFirstLast_returnLastFirst() throws Exception{
+		assertInverted("First Last", "Last, First");
+	}
+	
+	@Test
+	public void givenASimpleNameWithSpace_returnSimpleNameWithoutSpaces() throws Exception{
+		assertInverted(" Name ", "Name");
+	}
+	
+	@Test
+	public void givenFirstLastWithExtraSpaces_returnLastFirst() throws Exception{
+		assertInverted("   First   Last  ", "Last, First");
+	}
+	
+	@Test
+	public void ignoreHonorifics() throws Exception{
+		assertInverted("Mr. First Last", "Last, First");
+		assertInverted("Mrs. First Last", "Last, First");
+	}
+	
+	@Test
+	public void postNominals_stayAtEnd() throws Exception{
+		assertInverted("First Last Sr.", "Last, First Sr.");
+		assertInverted("First Last BS. Phd.", "Last, First BS. Phd.");
+	}
+	
+	@Test
+	public void integration() throws Exception{
+		assertInverted("  Robert  Martin  III  esq.   ", "Martin, Robert III esq.");
+	}
+
+	private String invertName(String name) {
+		if(name == null || name.length() <= 0) 
+			return "";
+		else 
+			return formatName(removeHonorifics(splitNames(name)));
+
+	}
+
+	private String formatName(List<String> names) {
+		if(names.size() == 1) {
+			return names.get(0);
+		} else {
+		return formatMultiElementName(names);
+		}
+	}
+
+	private String formatMultiElementName(List<String> names) {
+		String postNominal = getPostNominals(names);
+		String firstName = names.get(0);
+		String lastName = names.get(1);
+		String formattedName = String.format("%s, %s %s", lastName, firstName, postNominal).trim();
+		return formattedName;
+	}
+
+	private String getPostNominals(List<String> names) {
+		String postNominalString = "";
+		if(names.size() > 2) {
+			List<String> postNominals = names.subList(2, names.size());
+			for(String pn : postNominals)
+				postNominalString += pn + " ";
+		}
+		return postNominalString;
+	}
+
+	private List<String> removeHonorifics(List<String> names) {
+		if(names.size() > 1 && isHonorific(names.get(0)))
+			names.remove(0);
+		return names;
+	}
+
+
+	private boolean isHonorific(String word) {
+		return word.matches("Mr\\.|Mrs\\.");
+	}
+
+	private ArrayList<String> splitNames(String name) {
+		return new ArrayList<String>(Arrays.asList(name.trim().split("\\s+")));
 	}
 
 }
